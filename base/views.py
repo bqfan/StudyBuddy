429a21ed5e3a27e7f94e05729c21cccbe502a20f
Baiquan Fan <baiquan.fan@gmail.com>
2024-04-15 21:18:34 +0300

    add User registration.

--- base/views.py
+++ base/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from .models import Room, Topic
 from .forms import RoomForm
@@ -11,11 +12,13 @@ from django.db.models import Q
 
 # Create your views here.
 def loginPage(request):
+    page = "login"
+
     if request.user.is_authenticated:
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -31,7 +34,7 @@ def loginPage(request):
         else:
             messages.error(request, "Username or does not exist.")
 
-    context = {}
+    context = {'page': page}
 
     return render(request, "base/login_register.html", context)
 
@@ -108,3 +111,22 @@ def deleteRoom(request, pk):
         return redirect("home")
 
     return render(request, "base/delete.html", {"obj": room})
+
+
+def registerPage(request):
+    form = UserCreationForm
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+
+            return redirect("home")
+        else:
+            messages.error(request, "An error occurred during registration.")
+
+    return render(request, "base/login_register.html", {"form": form})
