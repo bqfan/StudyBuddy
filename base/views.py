1dbf41d857f71edb0aa8e650354f69fd1b5a197b
Baiquan Fan <baiquan.fan@gmail.com>
2024-04-17 20:31:57 +0300

    add Mobile Responsiveness.

--- base/views.py
+++ base/views.py
@@ -45,14 +45,14 @@ def logoutUser(request):
 
 
 def home(request):
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
 
     rooms = Room.objects.filter(
             Q(topic__name__icontains=q) |
             Q(name__icontains=q) |
             Q(description__icontains=q)
         )
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -208,3 +208,18 @@ def updateUser(request):
             return redirect("user-profile", pk=user.id)
 
     return render(request, "base/update_user.html", {"form": form})
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+
+    return render(request, "base/topics.html", {"topics": topics})
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+
+    return render(request,
+                  "base/activity.html",
+                  {"room_messages": room_messages})
